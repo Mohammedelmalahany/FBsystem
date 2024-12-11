713ab09634f3dbb36ca0f51e6d96d2d81a64be4a
w-hadi <wajdihadi96@gmail.com>
2024-12-11 23:59:00 +0200

    Completed Content class version 1

--- src/main/java/Content/Content.java
+++ src/main/java/Content/Content.java
@@ -2,7 +2,39 @@ package Content;
 import User.User;
 import java.util.*;
 public class Content {
-    static int iD;
+    //unique iD for each comment or post or react
+    public static int iD = 0;
     protected String content;
     protected List<User> likes, dislikes;
+    protected List<Comment> comments;
+
+    public Content (){
+        //unique iD for like or dislike
+        iD++;
+    }
+    public Content (String content){
+        //content of comment or post
+        this.content = content;
+        iD++;
+    }
+    public void addReactor(int react, User user){
+        if (react == 1){
+            likes.add(user);
+        }
+        if (react == 0){
+            dislikes.add(user);
+        }
+    }
+    public void removeReactor(User user){
+        if (likes.contains(user)){
+            likes.remove(user);
+        }
+        else if (dislikes.contains(user)){
+            dislikes.remove(user);
+        }
+    }
+    public void addComment(Comment comment){
+        comments.add(comment);
+    }
+
 }
