48a7ebb8444f87d25f9a1e676c695efbbf967161
BinAdam001 <168603066+BinAdam001@users.noreply.github.com>
2024-12-12 15:29:35 +0200

    Update Message.java

--- src/main/java/Message/Message.java
+++ src/main/java/Message/Message.java
@@ -1,11 +1,87 @@
-package Message;
-import Content.Content;
-import User.User;
-
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Message extends Content {
-    private int messageId;
-    private User sender;
-    private Date timestamp;
+public class message {
+    private static int idCounter = 0;
+    protected int messageId;
+    protected String content;
+    protected String sender;
+    protected Date timestamp;
+
+    public message(String content, String sender) {
+        this.messageId = ++idCounter;
+        this.content = content;
+        this.sender = sender;
+        this.timestamp = new Date();
+    }
+
+    public int getmessageId() {
+        return messageId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    // Method to format and display the date and time
+    public String getFormattedTimestamp() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(timestamp);
+    }
+    
+    public String toString() {
+        return
+                "\n Message From:'" + sender + '\'' +
+                "\n Message:'" + content + '\'' +
+                "\n Date/time:" + getFormattedTimestamp() + "\nMessageId:" + messageId +' ' ;
+    }
 }
+
+/*
+*main*
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do{
+
+            System.out.print("Enter your name: ");
+            String senderName = scanner.nextLine();
+            System.out.print("Enter your message: ");
+            String messageContent = scanner.nextLine();
+            message message = new message(messageContent, senderName);
+            System.out.println(message);
+            System.out.println("press one to send a new message:");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            while (true) {
+                try {
+                    System.out.println("Press 1 to send another message, or any other number to exit:");
+                    choice = scanner.nextInt();
+                    scanner.nextLine();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input! Please enter a valid number.");
+                    scanner.nextLine();
+                }
+            }
+
+        }while(choice ==1);
+
+        scanner.close(); // Close the scanner
+    }
+}*/
+
+
